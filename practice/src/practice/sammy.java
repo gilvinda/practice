342adbafd09dd21a86b203665beaebbbca192edd
pc <pc@DESKTOP-D8B2VD1>
2024-02-09 11:42:20 -0800

    commit practice

---
+++ practice/src/practice/sammy.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class sammy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my sammy is bad");
+
+	}
+
+}
