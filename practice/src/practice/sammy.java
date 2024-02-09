5a01cb7df3d90bfa4219297925bfe73cdc4a7f61
pc <pc@DESKTOP-D8B2VD1>
2024-02-09 11:51:37 -0800

    commit2

--- practice/src/practice/sammy.java
+++ practice/src/practice/sammy.java
@@ -1,12 +1,22 @@
 package practice;
 
 public class sammy {
-
+	String name;
+	String breed;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("my sammy is bad");
+		
+		//sammy(String name,String breed){
+			//this.name=name;
+			//this.breed=breed;
+		}
 
+	
+	
+	void eat() {
+		System.out.println("my sammy is good");
 	}
 
 }
